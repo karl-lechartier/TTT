bbd8307cfd7c29020770456601b5f9d7833c82a3
victor <victor.terrien@epsi.fr>
2023-10-02 17:47:57 +0200

    css màj

--- footer.php
+++ footer.php
@@ -2,5 +2,16 @@
 <html lang="fr">
 <body>
 <footer>
-    <i class="fa-brands fa-vimeo"></i>
+    <div class="footer-networks">
+        <a href="#"><i class="fa-brands fa-vimeo-v"></i></a>
+        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+        <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+    </div>
+    <div class="decoration-barre"></div>
+    <div class="footer-conditions">
+        <a href="#">Conditions d'utilisation</a>
+        <p>Copyright 2023 © TOUS DROITS RÉSERVÉS</p>
+    </div>
 </footer>
