979072e7af9eb7a076821920cd31f6b0999aaacb
doxka <karllechartier2@gmail.com>
2023-10-02 16:57:44 +0200

    le début header

---
+++ header_mini.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if (!isset($_SESSION['user'])){
+    $_SESSION['user']['role'] = 0;
+    $_SESSION['user']['id'] = null;
+    $_SESSION['user']['login'] = null;
+    $_SESSION['error'] = null;
+}
+$title="Gigaldi"
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $title ?></title>
+<link rel="stylesheet" href="style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&family=Lobster+Two:wght@400;700&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+          integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+</head>
