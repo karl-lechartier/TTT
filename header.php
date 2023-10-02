1beb928d86b5763cb7eb6fff763f4f5d9bd971b2
victor <victor.terrien@epsi.fr>
2023-10-02 16:35:21 +0200

    ajout font-family css

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&family=Lobster+Two:wght@400;700&display=swap" rel="stylesheet">
 <body>
 <header>
     <img src="img/logo-ttt.png" alt="Logo">
