29e4faa862fa14f64e658620ef8b301129687255
doxka <karllechartier2@gmail.com>
2023-10-03 13:46:59 +0200

    header + css

--- header.php
+++ header.php
@@ -1,17 +1,6 @@
 <?php
 include "header_mini.php";
 ?>
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $title ?></title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:wght@400;700&display=swap" rel="stylesheet">
-</head>
 <body>
 <nav>
     <input type="checkbox" id="nav-check">
