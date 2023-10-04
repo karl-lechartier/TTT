2a928de5279518917c50c031f750b92565cb9d91
escalope22 <victor.rambaud.85@gmail.com>
2023-10-04 09:56:09 +0200

    header mini source de style

--- header_mini.php
+++ header_mini.php
@@ -21,7 +21,7 @@ $title="Gigaldi"
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title ?></title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="scss/main.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&family=Lobster+Two:wght@400;700&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
