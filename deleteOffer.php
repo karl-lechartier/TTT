eee5eae5a06e751b9bc8e79e407440b8214c17d4
victor <victor.terrien@epsi.fr>
2023-10-04 15:34:47 +0200

    finish offer

--- deleteOffer.php
+++ deleteOffer.php
@@ -1,9 +1,39 @@
 <?php
 include "header.php";
-$id = $_GET['id'];
-echo $id;
-?>
+include_once "config.php";
+$pdo = new PDO("mysql:host=" . Config::SERVEUR . "; dbname=" . Config::BDO, Config::UTILISATEUR, Config::MOTDEPASSE);
 
+$id = $_GET['id'];
+
+if (!$_SESSION['user']['id']) {
+    header('location: index.php');
+}
+
+if (isset($_POST['supp'])) {
+    $token=filter_input(INPUT_POST, "token");
+    if($token!=$_SESSION["token"]){
+        die("Erreur de Token");
+    }
+
+    $id_offer = filter_input(INPUT_POST, "id_offer");
+
+    $requete = $pdo->prepare("DELETE FROM offer where id=:id");
+    $requete->bindParam(":id", $id_offer);
+    $requete->execute();
+
+    header('location: managed-shop.php');
+}
+
+$token = uniqid();
+$_SESSION["token"] = $token;
+?>
+    <main>
+        <form action="deleteOffer.php" method="post">
+            <input type="hidden" name="token" value="<?php echo $token ?>">
+            <input type="hidden" name="id_offer" value="<?php echo $id ?>">
+            <input type="submit" value="Valider la suppression" name="supp">
+        </form>
+    </main>
 <?php
 include "footer.php";
 ?>
