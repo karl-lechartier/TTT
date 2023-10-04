30c4b1c46c532626ce1d4beb0ca548123ea27559
victor <victor.terrien@epsi.fr>
2023-10-04 15:22:57 +0200

    3rd update offer

--- addOffer.php
+++ addOffer.php
@@ -20,14 +20,15 @@ if (isset($_POST['valider'])) {
     $date_start = filter_input(INPUT_POST, "date_start");
     $date_end = filter_input(INPUT_POST, "date_end");
     $points = filter_input(INPUT_POST, "points");
+    $shop = filter_input(INPUT_POST, "shop");
 
-    $requete = $pdo->prepare("insert into offer (id, title, description, date_start, date_end, points, shop_id) VALUES (NULL, :title, :description, :date_start, :date_end, :points, :shop_id)");
+    $requete = $pdo->prepare("INSERT INTO offer (id, title, description, date_start, date_end, points, shop_id) VALUES (NULL, :title, :description, :date_start, :date_end, :points, :shop_id)");
     $requete->bindParam(":title",$title);
     $requete->bindParam(":description",$description);
     $requete->bindParam(":date_start",$date_start);
     $requete->bindParam(":date_end",$date_end);
     $requete->bindParam(":points",$points);
-    $requete->bindParam(":shop_id",$id_shop);
+    $requete->bindParam(":shop_id",$shop);
     $requete->execute();
 
     header('location: managed-shop.php');
@@ -37,10 +38,11 @@ $token = uniqid();
 $_SESSION["token"] = $token;
 ?>
     <main>
-        <form action="addOffer.php?id=<?php echo $id_shop ?>" method="post">
+        <form action="addOffer.php" method="post">
             <input type="hidden" name="token" value="<?php echo $token ?>">
+            <input type="hidden" name="shop" value="<?php echo $id_shop ?>">
             <label for="title">Titre </label>
-            <input type="text" id="title" name="title" required >
+            <input type="text" id="title" name="title" required>
             <label for="description">Description </label>
             <textarea name="description" id="description" cols="30" rows="10" required></textarea>
             <label for="date_start">Date de début </label>
