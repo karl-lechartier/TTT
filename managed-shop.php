b331d90015a3193c79bb9780af53b8044079997f
escalope22 <victor.rambaud.85@gmail.com>
2023-10-05 12:22:31 +0200

    managed-shop

--- managed-shop.php
+++ managed-shop.php
@@ -7,14 +7,9 @@ $pdo = new PDO("mysql:host=" . Config::SERVEUR . "; dbname=" . Config::BDO, Conf
 
     <body>
     <main>
-        <a href="ajout_magasin.php">Ajouter un Magasin</a>
-        <?php
-        if ($_SESSION['user']['id'] == null ) {
-            echo "pas connecté";
-        } else {
-            echo "connecté";
-        }
-        ?>
+        <div class="button-ajout-magasin-container">
+            <a class="button" href="ajout_magasin.php">Ajouter un Magasin</a>
+        </div>
         <div class="card-deck">
             <?php
             $requete = $pdo->prepare("SELECT shop.*, user_shop.est_proprio FROM shop JOIN user_shop ON shop.id = user_shop.id_shop WHERE user_shop.id_user=:id_user");
@@ -24,19 +19,24 @@ $pdo = new PDO("mysql:host=" . Config::SERVEUR . "; dbname=" . Config::BDO, Conf
 
             foreach ($lignes as $l) {
                 ?>
-                    <div class="card">
-                        <a class="magasin no-link-style" href="shop.php?id=<?php echo $l['id'] ?>">
-                            <img class="card-img-top" src="img/<?php echo $l['photo']?>" alt="Card image cap">
-                            <div class="card-body">
-                                <h2 class="card-title"><?php echo $l['name']?></h2>
-                                <p class="card-text"><?php echo $l['category']?></p>
-                                <p class="card-text"><small class="text-muted"><?php echo $l['adress']?></small></p>
+                    <div class="card-container">
+                        <div class="card">
+                            <a class="magasin no-link-style" href="shop.php?id=<?php echo $l['id'] ?>">
+                                <img class="card-img-top" src="img/<?php echo $l['photo']?>" alt="Card image cap">
+                                <div class="card-body">
+                                    <h2 class="card-title"><?php echo $l['name']?></h2>
+                                    <p class="card-text"><?php echo $l['category']?></p>
+                                    <p class="card-text"><small class="text-muted"><?php echo $l['adress']?></small></p>
+                                </div>
+                            </a>
+                            <div class="button-container">
+                                <a class="button" href="deleteShop.php?shop=<?php echo $l['id'] ?>">Supprimer le magasin</a>
+                                <a class="button" href="updateShop.php?shop=<?php echo $l['id'] ?>">Modifier les informations</a>
+                                <a class="button" href="manageOffer.php?shop=<?php echo $l['id'] ?>">Gérer les offres</a>
                             </div>
-                        </a>
-                        <a href="deleteShop.php?shop=<?php echo $l['id'] ?>">Supprimer le magasin</a>
-                        <a href="updateShop.php?shop=<?php echo $l['id'] ?>">Modifier les informations</a>
-                        <a href="manageOffer.php?shop=<?php echo $l['id'] ?>">Gérer les offres</a>
+                        </div>
                     </div>
+
                 <?php
             }
             ?>
