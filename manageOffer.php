52e0b3123bfe6f7e0b65d0b701acccd429e1bda0
escalope22 <victor.rambaud.85@gmail.com>
2023-10-06 09:41:50 +0200

    manage offer

--- manageOffer.php
+++ manageOffer.php
@@ -25,27 +25,29 @@ foreach ($lignes as $l) {
 
 ?>
 <main>
-    <a href="addOffer.php?id_shop=<?php echo $id_shop?>">Ajouter une offre</a>
+    <div class="button-ajout-magasin-container">
+        <a href="addOffer.php?id_shop=<?php echo $id_shop?>" class="button">Ajouter une offre</a>
+    </div>
     <?php
     $requete = $pdo->prepare("select * from offer WHERE shop_id = :shop_id");
     $requete->bindParam(":shop_id", $id_shop);
     $requete->execute();
     $lignes = $requete->fetchAll();
-
-    foreach ($lignes as $l) {
-        ?>
-            <table>
-                <tr>
-                    <td><?php echo $l['title'] ?></td>
-                    <td><?php echo $l['description'] ?></td>
-                    <td><?php echo $l['date_start'] ?></td>
-                    <td><?php echo $l['date_end'] ?></td>
-                    <td><?php echo $l['points'] ?></td>
-                    <td><a href="updateOffer.php?id=<?php echo $l['id'] ?>">Modifier</a></td>
-                    <td><a href="deleteOffer.php?id=<?php echo $l['id'] ?>">Supprimer</a></td>
-                </tr>
-            </table>
+    ?>
+    <div class="cards-container">
+        <?php foreach ($lignes as $l) { ?>
+            <div class="card">
+                <h2><?php echo htmlspecialchars($l['title']); ?></h2>
+                <p><?php echo htmlspecialchars($l['description']); ?></p>
+                <p><?php echo htmlspecialchars($l['date_start']); ?></p>
+                <p><?php echo htmlspecialchars($l['date_end']); ?></p>
+                <h4><?php echo htmlspecialchars($l['points']); ?></h4>
+                <a href="updateOffer.php?id=<?php echo htmlspecialchars($l['id']); ?>" class="btn btn-update">Modifier</a>
+                <a href="deleteOffer.php?id=<?php echo htmlspecialchars($l['id']); ?>" class="btn btn-delete">Supprimer</a>
+            </div>
         <?php } ?>
+    </div>
+
 </main>
 <?php
 include "footer.php";
